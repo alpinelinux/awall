9223d0abde8b39d75d5460a1b578061f6d3cc29b
Timo Teräs <timo.teras@iki.fi>
2012-06-08 07:57:09 +0300

    OUTPUT chain needs stateful tracking also

--- awall/modules/filter.lua
+++ awall/modules/filter.lua
@@ -137,7 +137,7 @@ for i, family in ipairs({'inet', 'inet6'}) do
 		       opts=opts})
       end
    end
-   for i, chain in ipairs({'FORWARD', 'INPUT'}) do
+   for i, chain in ipairs({'FORWARD', 'INPUT', 'OUTPUT'}) do
       table.insert(defrules,
 		   {family=family,
 		    table='filter',
