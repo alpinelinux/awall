c4e427e67b7c7f62598fa33e54b950b98a1f9d06
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2012-06-21 13:13:34 +0000

    masquerading set rule applied after other SNAT rules

--- awall/modules/masquerade.lua
+++ awall/modules/masquerade.lua
@@ -10,7 +10,9 @@ module(..., package.seeall)
 classes = {}
 
 -- TODO configuration of the ipset via JSON config
-defrules = {pre={{family='inet', table='nat', chain='POSTROUTING',
-		  opts='-m set --match-set awall-masquerade src -j awall-masquerade'},
-		 {family='inet', table='nat', chain='awall-masquerade',
-		  opts='-m set ! --match-set awall-masquerade dst -j MASQUERADE'}}}
+defrules = {['post-snat']={{family='inet', table='nat',
+			    chain='POSTROUTING',
+			    opts='-m set --match-set awall-masquerade src -j awall-masquerade'},
+			   {family='inet', table='nat',
+			    chain='awall-masquerade',
+			    opts='-m set ! --match-set awall-masquerade dst -j MASQUERADE'}}}
