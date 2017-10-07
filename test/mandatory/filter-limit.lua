e3798fb682ef5819d21796644b051ad262e7317d
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2017-10-07 12:24:30 +0300

    test: filter-limit: adjust indentation

--- test/mandatory/filter-limit.lua
+++ test/mandatory/filter-limit.lua
@@ -5,25 +5,25 @@ res = {}
 
 function add(limit_type, base)
    for _, count in ipairs{1, 30} do
-     for _, limit in ipairs{
-        count, {count=count, log=false}, {count=count, log='none'}
-     } do
-        for _, log in ipairs{false, true, 'none'} do
-           for _, action in ipairs{false, 'pass'} do
-	      if not (count == 30 and log and action) then
-	         table.insert(
-	            res,
-		    util.update(
-		       util.copy(base or {}),
-		       {
-		          [limit_type..'-limit']=limit,
-		          log=log or nil,
-		          action=action or nil
-		       }
-		    )
-                 )
-             end
-	   end
+      for _, limit in ipairs{
+         count, {count=count, log=false}, {count=count, log='none'}
+      } do
+         for _, log in ipairs{false, true, 'none'} do
+            for _, action in ipairs{false, 'pass'} do
+	       if not (count == 30 and log and action) then
+	          table.insert(
+	             res,
+		     util.update(
+		        util.copy(base or {}),
+		        {
+		           [limit_type..'-limit']=limit,
+		           log=log or nil,
+		           action=action or nil
+		        }
+		     )
+                  )
+               end
+	    end
 	 end
       end
    end
