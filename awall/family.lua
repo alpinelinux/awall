9c932a749e28eb8d68f14234f35c5fdbde02d124
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2018-12-14 19:32:53 +0200

    family: list of families enabled in kernel

--- awall/family.lua
+++ awall/family.lua
@@ -4,4 +4,13 @@ Copyright (C) 2012-2019 Kaarle Ritvanen
 See LICENSE file for license details
 ]]--
 
-return {ALL={'inet', 'inet6'}}
+local M = {ACTIVE={}, ALL={}}
+
+local stat = require('posix').stat
+
+for family, procfile in pairs{inet='raw', inet6='raw6'} do
+   table.insert(M.ALL, family)
+   if stat('/proc/net/'..procfile) then table.insert(M.ACTIVE, family) end
+end
+
+return M
