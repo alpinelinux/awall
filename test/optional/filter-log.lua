dde0cffd637b0a615746b28d0158ad941ef116ac
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2020-07-10 15:30:39 +0300

    test: split filter-log cases

---
+++ test/optional/filter-log.lua
@@ -0,0 +1,19 @@
+--[[
+Filter log test cases for Alpine Wall
+Copyright (C) 2012-2020 Kaarle Ritvanen
+See LICENSE file for license details
+]]--
+
+
+json = require('cjson')
+
+res = {}
+
+for _, log in ipairs{'', false, true, 'dual', 'mirror', 'none', 'ulog'} do
+   for _, action in ipairs{false, 'drop', 'pass'} do
+      if log == '' then log = nil end
+      table.insert(res, {log=log, action=action or nil})
+   end
+end
+
+print(json.encode{filter=res})
